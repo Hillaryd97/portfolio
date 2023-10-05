f82fd64bc5b9942aa48063229917437aa7d7556f
Simi Dickson <45945566+Hillaryd97@users.noreply.github.com>
2023-10-05 10:15:54 +0100

    Update ProjectItem.jsx

--- src/components/ProjectItem.jsx
+++ src/components/ProjectItem.jsx
@@ -17,11 +17,11 @@ const ProjectItem = ({
       whileTap={{ scale: 0.9 }}
       transition={{ type: "spring", stiffness: 400, damping: 17 }}
     >
-      <div className="w-fit ">
+      <div className="w-fit">
         <img src={preview} alt="Project Image" />
         <div className="lg:p-4 p-1.5 absolute top-0 left-0 w-full h-0 flex flex-col justify-center items-center bg-opacity-80 opacity-0 bg-black group-hover:h-full group-hover:opacity-100 duration-500">
           <p className="lg:text-xl text-base font-bold lg:pb-2">{projectName}</p>
-          <p className="lg:text-md text-sm text-center">{description}</p>
+          <p className="md:text-md text-xs text-center">{description}</p>
           <div className="flex flex-row space-x-2 lg:space-x-4">
             <div className="flex flex-row space-x-1 items-center py-4 cursor-pointer text-white hover:text-blue-400 duration-300">
               {/* <BsFillEyeFill size={15} /> */}
