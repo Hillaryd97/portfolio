2208aed8b82e2ee290df08859d1a99ffd59f3bba
Hillary <Hillaryd97@gmail.com>
2023-10-11 21:13:36 +0100

    .

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -15,43 +15,43 @@ const About = () => {
       </p>
       <p className="text-light-gray text-xl font-bold py-4">Technologies I work with: </p>
       <div className="text-white grid grid-cols-3 gap-4 lg:gap-5 lg:grid-cols-4">
-        <div className="flex items-center space-x-1 p-2 w-fit bg-dark-blue">
+        <div className="flex items-center space-x-1 p-2 w-full bg-dark-blue">
           <AiFillHtml5 size={30} />
           <p>HTML</p>
         </div>{" "}
-        <div className="flex items-center space-x-1 p-2 w-fit bg-dark-blue">
+        <div className="flex items-center space-x-1 p-2 w-full bg-dark-blue">
           <DiCss3 size={30} />
           <p>CSS</p>
         </div>{" "}
-        <div className="flex items-center space-x-1 p-2 w-fit bg-dark-blue">
+        <div className="flex items-center space-x-1 p-2 w-full bg-dark-blue">
           <DiJavascript1 size={30} />
           <p>JavaScript</p>
         </div>{" "}
-        <div className="flex items-center space-x-1 p-2 w-fit bg-dark-blue">
+        <div className="flex items-center space-x-1 p-2 w-full bg-dark-blue">
           <SiPhp size={30} />
           <p>PHP</p>
         </div>{" "}
-        <div className="flex items-center space-x-1 p-2 w-fit bg-dark-blue">
+        <div className="flex items-center space-x-1 p-2 w-full bg-dark-blue">
           <SiTailwindcss size={30} />
           <p>Tailwind</p>
         </div>{" "}
-        <div className="flex items-center space-x-1 p-2 w-fit bg-dark-blue">
+        <div className="flex items-center space-x-1 p-2 w-full bg-dark-blue">
           <FaReact size={30} />
           <p>React.js</p>
         </div>
-        <div className="flex items-center space-x-1 p-2 w-fit bg-dark-blue">
+        <div className="flex items-center space-x-1 p-2 w-full bg-dark-blue">
           <SiSupabase size={30} />
           <p>Supabase</p>
         </div>{" "}
-        <div className="flex items-center space-x-1 p-2 w-fit bg-dark-blue">
+        <div className="flex items-center space-x-1 p-2 w-full bg-dark-blue">
           <FaBootstrap size={30} />
           <p>Bootstrap</p>
         </div>{" "}
-        <div className="flex items-center space-x-1 p-2 w-fit bg-dark-blue">
+        {/* <div className="flex items-center space-x-1 p-2 w-full bg-dark-blue">
           <DiSqllite size={30} />
           <p>SQL</p>
-        </div>
-        <div className="flex items-center space-x-1 p-2 w-fit bg-dark-blue">
+        </div> */}
+        <div className="flex items-center space-x-1 p-2 w-full bg-dark-blue">
           <FaFigma size={30} />
           <p>Figma</p>
         </div>
--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,4 +1,4 @@
-import { useRef } from "react";
+import React, { useRef, useState } from "react";
 import emailjs from "emailjs-com";
 import { FaGithub, FaLinkedin, FaTwitter } from "react-icons/fa";
 import { SiGmail } from "react-icons/si";
@@ -6,6 +6,8 @@ import { motion } from "framer-motion";
 
 const Contact = () => {
   const form = useRef();
+  const [isMessageSent, setIsMessageSent] = useState(false);
+  const [buttonText, setButtonText] = useState("Send Message");
 
   const sendEmail = (e) => {
     e.preventDefault();
@@ -20,29 +22,49 @@ const Contact = () => {
       .then(
         (result) => {
           console.log(result.text);
+          setButtonText("Sent"); // Change button text to "Sent"
+          setTimeout(() => {
+            setButtonText("Send Message"); // Reset button text after a few seconds
+          }, 3000); // Change the duration as needed (3 seconds in this example)
         },
         (error) => {
           console.log(error.text);
         }
       );
+
     e.target.reset();
   };
+
   return (
     <div id="contact" className="text-left text-white mb-8">
       <h2 className="text-light-gray text-xl font-bold py-4">CONTACT ME!</h2>
-      <p className="pb-3">Have a job for me? Reach me through any of the mediums below!</p>
+      <p className="pb-3">
+        Have a job for me? Reach me through any of the mediums below!
+      </p>
       <div className="lg:grid lg:grid-cols-4 flex flex-col-reverse">
         <div className="flex lg:flex-col items-center  justify-evenly  py-3">
-          <a href="https://twitter.com/hik_ari_" className="hover:text-blue-600 duration-300">
+          <a
+            href="https://twitter.com/hik_ari_"
+            className="hover:text-blue-600 duration-300"
+          >
             <FaTwitter size={30} />
           </a>
-          <a href="https://github.com/Hillaryd97" className="hover:text-blue-600 duration-300">
+          <a
+            href="https://github.com/Hillaryd97"
+            className="hover:text-blue-600 duration-300"
+          >
             <FaGithub size={30} />
           </a>
-          <a href="https://www.linkedin.com/in/simidickson" className="hover:text-blue-600 duration-300">
+          <a
+            href="https://www.linkedin.com/in/simidickson"
+            className="hover:text-blue-600 duration-300"
+          >
             <FaLinkedin size={30} />
           </a>
-          <a href="mailto:Hillaryd97@gmail.com" className="hover:text-blue-600 duration-300">
+          <a
+            href="mailto:Hillaryd97@gmail.com"
+            className="hover:text-blue-600 duration-300"
+          >
             <SiGmail size={30} />
           </a>
         </div>
@@ -79,8 +101,14 @@ const Contact = () => {
             className="px-2 py-1.5 rounded-sm bg-blue-500 text-white"
             type="submit"
           >
-            Send Message
+            {buttonText}
           </motion.button>
+
+          {/* {isMessageSent && (
+            <div className="text-center font-bold text-green-500">
+              Message Sent!
+            </div>
+          )} */}
         </form>
       </div>
     </div>
